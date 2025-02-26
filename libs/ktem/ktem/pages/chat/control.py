ca8e69fea1cf53d5d0f5d0e1d26a72267dba42be
Jason Christopher <jchristopheris@gmail.com>
2025-02-25 17:22:12 -0800

    fix: clearer label for share / public conversation

--- libs/ktem/ktem/pages/chat/control.py
+++ libs/ktem/ktem/pages/chat/control.py
@@ -88,7 +88,7 @@ class ConversationControl(BasePage):
         with gr.Row() as self._new_delete:
             self.cb_is_public = gr.Checkbox(
                 value=False,
-                label="JVolta Public",
+                label="JV Public",
                 min_width=10,
                 scale=4,
                 elem_id="is-public-checkbox",
